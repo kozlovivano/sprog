8a081e651a42eaa376ddcbfd5795ae167f70dcb4
miller-alexey <davidenko.alexey@outlook.com>
2019-03-15 03:45:41 +1000

    Boilderplate Heroku deploy

--- app.js
+++ app.js
@@ -2,20 +2,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 // MongoDB
 const mongoose = require('mongoose');
+
 // .env configuration
 require('dotenv').config();
+
 // Constants
 const port = process.env.PORT || 8000;
 const development = process.env.NODE_ENV !== 'production';
 const ROOT_URL = development ? `http://localhost:${port}` : process.env.DEPLOY_URL;
 const MONGO_URL = development ? process.env.LOCAL_MONGO_URL : process.env.MLAB_URL;
+
 const option = {
 	useNewUrlParser: true
 };
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
