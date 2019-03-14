30e60e8aecc519d58de87bee206fa612b9758c99
miller-alexey <davidenko.alexey@outlook.com>
2019-03-15 03:32:18 +1000

    Boilerplate

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-
+// Express server
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+// MongoDB
+const mongoose = require('mongoose');
+// .env configuration
+require('dotenv').config();
+// Constants
+const port = process.env.PORT || 8000;
+const development = process.env.NODE_ENV !== 'production';
+const ROOT_URL = development ? `http://localhost:${port}` : process.env.DEPLOY_URL;
+const MONGO_URL = development ? process.env.LOCAL_MONGO_URL : process.env.MLAB_URL;
+const option = {
+	useNewUrlParser: true
+};
 
 const app = express();
-const port = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+mongoose.connect(MONGO_URL, option).then(
+	() 	=> { console.log('DB connection established!'); },
+	err => { console.log('DB connection failed. Error: ', err); }
+);
+
+if (!development) {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	// Handle React routing, return all requests to React app
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`> Ready on ${ROOT_URL}`));
