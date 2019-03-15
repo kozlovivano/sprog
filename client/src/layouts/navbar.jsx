6cbabd6a7dac39421bfda5514e974a64b80a3f5f
miller-alexey <davidenko.alexey@outlook.com>
2019-03-16 03:53:29 +1000

    Navbar added

---
+++ client/src/layouts/navbar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from '../assets/png/logo.png';
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+
+        };
+    }
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm bg-blue navbar-dark">
+                <a className="navbar-brand" href="/"><img src={logo} alt="Sprog logo" width="100"></img></a>
+                <button 
+                    className="navbar-toggler collapsed" 
+                    type="button" data-toggle="collapse" 
+                    data-target="#navbarCollapse" aria-controls="navbarCollapse" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="navbar-collapse collapse" id="navbarCollapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/aboutus">About Us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/faq">FAQ's</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contactus">Contact Us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/login">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
